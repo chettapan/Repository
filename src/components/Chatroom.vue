825326fc826e553ae9b9bf133406288c887258f9
chettapan <chettapan.n58@rsu.ac.th>
2020-02-23 06:35:09 +0700

    06.35

--- src/components/Chatroom.vue
+++ src/components/Chatroom.vue
@@ -4,12 +4,14 @@
 			<div class="message" v-for="message in messages" v-bind:key="message._id">
 				<div class="username">{{message.username}}</div>
 				<div class="message-text">{{message.msg}}</div>
+				<div>{{message.time}}</div>
 			</div>
 		</div>
 		<form class="input-container" v-on:submit="sendMessage">
 			<input type="text" v-model="msg">
 			<button v-on:click="sendMessage" v-bind:disabled="!msg">Send</button>
 		</form>
+		
 	</div>
 </template>
 
@@ -23,6 +25,7 @@ export default {
 		}
 	},
 	methods: {
+		
 		sendMessage: function () {
 			if (!this.msg) {
 				alert("Please enter a message");
